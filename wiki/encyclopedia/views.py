06a21ab59704e0cb0be8c4e014627c748efe0307
calnet-pc1 <johnmunn78@gmail.com>
2022-03-09 19:09:40 +0000

    current changes

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,45 @@
-from django.shortcuts import render
+# from django import forms
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, reverse
 
 from . import util
 
+# class SearchForm(forms.Form):
+#     search = forms.CharField()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "entry": util.get_entry(title)
+    })
+
+def search(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+
+        query = request.POST["q"]
+
+        # If the query matches the name of an encyclopedia entry, the user should be redirected to that entry’s page.
+        if util.get_entry(query):
+            return HttpResponseRedirect(query)
+        else:
+            # If the query does not match the name of an encyclopedia entry, 
+            # the user should instead be taken to a search results page that displays a list 
+            # of all encyclopedia entries that have the query as a substring. 
+            # For example, if the search query were ytho, then Python should appear in the search results.
+            return render(request, "encyclopedia/search.html", {
+                "debug": request.POST,
+                "results": query
+            })
+
+
+    # if a GET (or any other method) we'll redirect to home page
+    else:
+        return HttpResponseRedirect(reverse("index"))
+
+    
