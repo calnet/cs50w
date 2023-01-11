e6c98084b95ec9c2c82e4211fb1a1ea1d44a083a
John Munn <johnmunn78@gmail.com>
2023-01-11 20:48:57 +0000

    Update Database

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Bid, Comment, Listing, User, Watchlist
+from .models import Bid, Comment, Listing, User, Watchlist, Category
 
 # Register your models here.
 admin.site.register(Bid)
 admin.site.register(Comment)
+admin.site.register(Category)
 admin.site.register(Listing)
 admin.site.register(User, UserAdmin)
 admin.site.register(Watchlist)
