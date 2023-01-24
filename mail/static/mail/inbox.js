e1bd78e97872ea9c4c121a2936d6afd5d7020450
John Munn <johnmunn78@gmail.com>
2023-01-24 20:26:48 +0000

    Reply and Archive features implemented

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -23,14 +23,27 @@ function compose_email(data) {
 	document.querySelector('#compose-body').value = '';
 
 	// Check if reply email
-	if (data instanceof Event === false)  {
+	if (data instanceof Event === false) {
 		const compose_view = document.querySelector('#compose-view');
 		const heading = compose_view.getElementsByTagName('h3')[0];
 		heading.innerHTML = 'Reply Email';
 
 		// Populate composition fields
 		document.querySelector('#compose-recipients').value = data.sender;
-		document.querySelector('#compose-subject').value = 'Re: ' + data.subject;
+		
+		const subject = document.querySelector('#compose-subject');
+		// Prefix Subject Line
+		if (data.subject.startsWith('Re: ')) {
+			subject.value = data.subject;
+		} else {
+			subject.value = 'Re: ' + data.subject;
+		}
+
+		const body = document.querySelector('#compose-body');
+
+		body.rows = 20;
+		body.value = `On ${data.timestamp}, <${data.sender}> wrote: \n\n${data.body}`;
+
 	}
 
 	// Listen for form submit action
@@ -53,7 +66,7 @@ function load_mailbox(mailbox) {
 	get_mailbox_emails(mailbox);
 }
 
-function load_email(email) {
+function load_email(email, mailbox) {
 	// Show the email and hide other views
 	document.querySelector('#email-view').style.display = 'block';
 	document.querySelector('#emails-view').style.display = 'none';
@@ -62,26 +75,26 @@ function load_email(email) {
 	// Show the subject line
 	document.querySelector('#email-view').innerHTML = `<h3>${email.subject}</h3>`;
 
-	display_email(email);
+	display_email(email, mailbox);
 
-	// mark_email_read(email.id);
+	mark_email_read(email.id);
 }
 
-function open_email() {
+function open_email(element, mailbox) {
 	// Display email details
-	id = parseInt(this.id.split("data-")[1]);
-	console.log('This element has been clicked! - ' + this.id);
-	console.log('Display email contents!');
-	
-	get_email(id);
+	const id = parseInt(element.id.split("data-")[1]);
+	// console.log('This element has been clicked! - ' + id);
+	// console.log('Display email contents!');
+
+	get_email(id, mailbox);
 }
 
-function display_email(email) {
-	display_email_headings(email);
-	display_email_body(email);
+function display_email(email, mailbox) {
+	display_email_headings(email, mailbox);
+	display_email_body(email, mailbox);
 }
 
-function display_email_headings(email) {
+function display_email_headings(email, mailbox) {
 	const email_view = document.querySelector('#email-view');
 
 	// Construct container layout to display email
@@ -90,9 +103,9 @@ function display_email_headings(email) {
 
 	// Apply CSS Styles
 	email_container.classList.add('border');
-	
+
 	email_view.appendChild(email_container);
-	
+
 	// const email_header_labels = [];
 	const email_headers = document.createElement('div');
 	email_headers.id = 'email_headers';
@@ -103,18 +116,18 @@ function display_email_headings(email) {
 	labels.forEach(label => {
 		const email_header = document.createElement('div');
 		email_header.id = 'email_header_' + label.toLowerCase();
-		
+
 		const email_header_label = document.createElement('span');
 		email_header_label.id = 'email_header_label_' + label.toLowerCase();
 		email_header_label.innerHTML = label + ': ';
 		email_header.append(email_header_label);
-		
+
 		const email_header_data = document.createElement('span');
 		email_header_data.id = 'email_header_data_' + label.toLowerCase();
 		email_header.append(email_header_data);
-		
+
 		email_headers.append(email_header);
-		
+
 		// Apply CSS Styles to Header
 		email_header.classList.add('email_header');
 		email_header.classList.add('row');
@@ -127,73 +140,93 @@ function display_email_headings(email) {
 	});
 
 	email_container.append(email_headers);
-	
+
 	let element = document.querySelector('#email_header_from');
 	element.lastChild.innerHTML = email.sender
-	
+
 	element = document.querySelector('#email_header_to');
 	if (email.recipients.length > 0) {
 		element.lastChild.innerHTML = email.recipients;
 	}
-	
+
 	element = document.querySelector('#email_header_subject');
 	element.lastChild.innerHTML = email.subject;
 
 	element = document.querySelector('#email_header_timestamp');
 	element.lastChild.innerHTML = email.timestamp;
 
-	// for (let i = 0; i < email_header_labels.length; i++) {
-	// 	const element = document.querySelector('#' + email_header_labels[i].id)
-	// 	email_header.innerHTML = email_header_labels[i].innerHTML + ': ' + email_headers[i];
-	// }
-	
-	// email_container.appendChild(email_headers);
-
-	const reply = document.createElement('button');
-	reply.id = 'reply';
-
-	reply.classList.add('mt-3');
-	reply.classList.add('btn');
-	reply.classList.add('btn-sm');
-	reply.classList.add('btn-outline-primary');
-
-	reply.innerHTML = 'Reply';
-
-	reply.addEventListener('click', () => {
+	element = create_element('button', 'reply', 'Reply', ['btn', 'btn-sm', 'btn-outline-primary']);
+	element.addEventListener('click', () => {
 		const data = {
 			'id': email.id,
-			'sender':email.sender,
-			'subject':email.subject
-		}
-		
+			'sender': email.sender,
+			'subject': email.subject,
+			'timestamp': email.timestamp,
+			'body': email.body
+		};
+
 		compose_email(data);
 	});
 
-	email_headers.append(reply);
+	email_headers.append(element);
+
+	if (mailbox === 'inbox') {
+		element = create_element('button', 'archive', 'Archive', ['btn', 'btn-sm', 'btn-outline-primary']);
+		element.addEventListener('click', () => {
+			archive_email(email.id);
+		});
+	} else if (mailbox === 'archive') {
+		element = create_element('button', 'archive', 'Move to Inbox', ['btn', 'btn-sm', 'btn-outline-primary']);
+		element.addEventListener('click', () => {
+			unarchive_email(email.id);
+		});
+	}
+
+	email_headers.append(element);
 }
 
-function display_email_body(email) {
+/**
+ * Creates an instance of the element for the specified tag.
+ * @param tagName The name of the element.
+ * @param id The id of the element.
+ * @param innerHTML A string to be used as the text value for the element.
+ * @param classList An array of CSS class names to be applied to the element.
+ */
+function create_element(tagName, id = '', innerHTML = '', classList = []) {
+	const element = document.createElement(tagName);
+
+	classList.forEach(class_name => {
+		element.classList.add(class_name);
+	});
+
+	element.id = id;
+	element.innerHTML = innerHTML;
+
+	return element;
+}
+
+function display_email_body(email, mailbox) {
 	const email_container = document.querySelector('#email_container');
 	const email_body = document.createElement('div');
 	email_body.id = 'email_body';
-	
+
 	// Apply CSS Styles
 	email_body.classList.add('border');
 	email_body.classList.add('p-3');
-	
+
 	email_body.innerHTML = email.body;
-	
+
 	email_container.append(email_body);
 }
 
-function display_mailbox_emails(mailbox, emails) {
-	display_mailbox_headings(mailbox, emails);
-	
+function display_mailbox_emails(emails, mailbox) {
+	display_mailbox_headings(emails, mailbox);
+
 	const mailbox_container = document.querySelector('#mailbox_container');
-	
+
 	const mailbox_emails = document.createElement('div')
 	mailbox_emails.id = 'mailbox_emails';
-	
+
 	mailbox_container.append(mailbox_emails);
 
 	emails.forEach(email => {
@@ -209,33 +242,35 @@ function display_mailbox_emails(mailbox, emails) {
 			if (column === (mailbox === 'sent' ? 'recipients' : 'sender') || column === 'subject' || column === 'timestamp') {
 				if (Object.hasOwnProperty.call(email, column)) {
 					row.appendChild(document.createElement('div'));
-					
+
 					row.lastChild.id = 'mailbox-emails-data-' + column + '-' + email.id;
-					
+
 					// Apply CSS Classes
 					row.lastChild.classList.add('col');
 					row.lastChild.classList.add('border');
 					row.lastChild.classList.add('p-1');
 					row.lastChild.classList.add('ps-3');
-					
+
 					// Apply CSS Class if email is read
 					if (email.read) {
 						row.lastChild.classList.add("bg-secondary");
 						row.lastChild.classList.add("bg-opacity-25");
 						row.lastChild.classList.add("border-dark-subtle");
 					}
-					
+
 					row.lastChild.innerHTML = email[column];
 					mailbox_emails.append(row);
 				}
 			}
 		}
-		
-		row.addEventListener('click', open_email);
+
+		row.addEventListener('click', function (e) {
+			open_email(this, mailbox);
+		});
 	});
-	
+
 	// Fix borders for last row in email list
-	if (mailbox_emails.childElementCount > 0){
+	if (mailbox_emails.childElementCount > 0) {
 		for (let i = 0; i < mailbox_emails.lastChild.childElementCount; i++) {
 			const element = mailbox_emails.lastChild.children[i];
 			// 1st Column
@@ -250,9 +285,9 @@ function display_mailbox_emails(mailbox, emails) {
 	}
 }
 
-function display_mailbox_headings(mailbox, emails) {
+function display_mailbox_headings(emails, mailbox) {
 	const emails_view = document.querySelector('#emails-view');
-	
+
 	// Construct container layout to display emails
 	const mailbox_container = document.createElement('div');
 	mailbox_container.id = 'mailbox_container';
@@ -264,7 +299,7 @@ function display_mailbox_headings(mailbox, emails) {
 	mailbox_container.classList.add('text-start');
 
 	emails_view.appendChild(mailbox_container);
-	
+
 	const mailbox_headings = document.createElement('div');
 	mailbox_headings.id = 'mailbox_headings';
 
@@ -273,25 +308,28 @@ function display_mailbox_headings(mailbox, emails) {
 	mailbox_headings.classList.add('border-dark');
 
 	mailbox_container.appendChild(mailbox_headings);
-	
+
 	// Get the field labels
 	let column_names = [];
 
 	if (emails.length > 0) {
 		column_names = Object.keys(emails[0]);
+	} else {
+		console.log('Display ' + mailbox + ' emails successful - No records Found');
+		return;
 	}
-	
+
 	// Get header row for table
 	column_names.forEach(column_name => {
 		if (column_name === (mailbox === 'sent' ? 'recipients' : 'sender') || column_name === 'subject' || column_name === 'timestamp') {
 			const element = document.createElement('div');
 			element.id = 'mailbox_header_' + column_name;
-			
+
 			element.classList.add('p-2');
-			
+
 			column_name = column_name.charAt(0).toUpperCase() + column_name.slice(1);
 			element.innerHTML = column_name;
-			
+
 			mailbox_headings.append(element);
 		}
 	});
@@ -306,7 +344,7 @@ function display_mailbox_headings(mailbox, emails) {
 		element.classList.add('text-center');
 		element.classList.add("bg-secondary");
 		element.classList.add("bg-opacity-50");
-		
+
 		// Fix borders for header row
 		// 1st Column
 		if (i === 0) {
@@ -323,24 +361,46 @@ function display_mailbox_headings(mailbox, emails) {
 	}
 }
 
-function get_mailbox_emails(mailbox) {
+async function get_mailbox_emails(mailbox) {
 	const url = '/emails/' + mailbox;
 
-	fetch(url)
-		.then(response => response.json())
-		.then(emails => {
-			display_mailbox_emails(mailbox, emails);
-		});
+	let response = await fetch(url);
+
+	if (response.status == 200) {
+		let emails = await response.json();
+		
+		display_mailbox_emails(emails, mailbox)
+
+		console.log('Display ' + mailbox + ' emails successful - Response: ' + response.status + ' received');
+	} else {
+		console.log('Display ' + mailbox + ' emails failed - Response: ' + response.status + ' received');
+	}
 }
 
-function get_email(id) {
+// function get_email(id, mailbox) {
+// 	const url = '/emails/' + id;
+
+// 	fetch(url)
+// 		.then(response => response.json())
+// 		.then(email => {
+// 			load_email(email, mailbox);
+// 		});
+// }
+
+async function get_email(id, mailbox) {
 	const url = '/emails/' + id;
 
-	fetch(url)
-		.then(response => response.json())
-		.then(email => {
-			load_email(email);
-		});
+	let response = await fetch(url);
+
+	if (response.status == 200) {
+		let email = await response.json();
+
+		load_email(email, mailbox);
+
+		console.log('Load ' + mailbox + ' email id: ' + id + ' successful - Response: ' + response.status + ' received');
+	} else {
+		console.log('Load ' + mailbox + ' email id: ' + id + ' failed - Response: ' + response.status + ' received');
+	}
 }
 
 function send_mail() {
@@ -367,27 +427,60 @@ function send_mail() {
 		});
 }
 
-function update_email(id, field, value) {
-	fetch('/emails/' + id, {
+async function update_email(id, field, value) {
+	let response = await fetch('/emails/' + id,
+	{
 		method: 'PUT',
 		body: JSON.stringify({
 			[field]: value
 		})
-	})
+	});
+	
+	if (response.status = 204) {
+		console.log('Update email: '+ id +' field: '+ field +' with '+ value +' was successful - Response: ' + response.status + ' received');
+		return response;
+	} else {
+		console.log('Update email: '+ id +' field: '+ field +' with '+ value +' failed - Response: ' + response.status + ' received');
+		return response;
+	}
 }
 
-function mark_email_read(id) {
-	update_email(id, 'read', true)
+async function mark_email_read(id) {
+	let response = await update_email(id, 'read', true)
+
+	if (response.status = 204) {
+		console.log('Mark email id: '+ id +' read was successful - Response: ' + response.status + ' received');
+		// return response;
+	} else {
+		console.log('Mark email id: '+ id +' read failed - Response: ' + response.status + ' received');
+		// return response;
+	}
 }
 
-function mark_email_unread(id) {
-	update_email(id, 'read', false)
+async function mark_email_unread(id) {
+	let response = await update_email(id, 'read', false)
+
+	if (response.status = 204) {
+		console.log('Mark email id: '+ id +' unread was successful - Response: ' + response.status + ' received');
+		// return response;
+	} else {
+		console.log('Mark email id: '+ id +' unread failed - Response: ' + response.status + ' received');
+		// return response;
+	}
 }
 
-function archive_email(id) {
-	update_email(id, 'archived', true)
+async function archive_email(id) {
+	let response = await update_email(id, 'archived', true)
+	
+	if (response.status = 204) {
+		load_mailbox('inbox');
+	}
 }
 
-function unarchive_email(id) {
-	update_email(id, 'archived', false)
+async function unarchive_email(id) {
+	let response = await update_email(id, 'archived', false)
+	
+	if (response.status = 204) {
+		load_mailbox('inbox');
+	}
 }
