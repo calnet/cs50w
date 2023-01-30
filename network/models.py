1531d571b5d56bbd8fb9bd3ff78b9fb283f91a84
John Munn <johnmunn78@gmail.com>
2023-01-30 19:19:27 +0000

    Add Post model

--- network/models.py
+++ network/models.py
@@ -4,3 +4,17 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey(
+        "User", on_delete=models.CASCADE, related_name="posts")
+    body = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "body": self.body,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p")
+        }
