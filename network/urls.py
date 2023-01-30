e54f02d7047a945c0eb6d4c18cbc571bff176093
John Munn <johnmunn78@gmail.com>
2023-01-30 19:21:39 +0000

    Add view for compose_post

--- network/urls.py
+++ network/urls.py
@@ -1,4 +1,3 @@
-
 from django.urls import path
 
 from . import views
@@ -7,5 +6,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+
+    # API Routes
+    path("posts", views.compose_post, name="compose_post"),
 ]
--- network/views.py
+++ network/views.py
@@ -1,15 +1,33 @@
+import json
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
-from .models import User
+from .models import Post, User
 
 
 def index(request):
     return render(request, "network/index.html")
 
+@csrf_exempt
+@login_required
+def compose_post(request):
+
+    # Composing a new post must be via POST
+    if request.method != "POST":
+        return JsonResponse({"error": "POST request required."}, status=400)
+
+    # Check post
+    data = json.loads(request.body)
+
+    # Get contents of post
+    body = data.get("body", "")
+
+    return JsonResponse({"message": "Post submitted successfully."}, status=201)
 
 def login_view(request):
     if request.method == "POST":
