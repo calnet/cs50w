d7b492d9d55fe6a36cf2684227c366c44653eff3
John Munn <johnmunn78@gmail.com>
2023-09-13 19:53:40 +0100

    Modify GlobalCssOverride

---
+++ frontend/src/GlobalCssOverride.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyles } from '@mui/material';
+
+function GlobalCssOverride() {
+    return (
+        <>
+            <GlobalStyles styles={{}} />
+        </>
+    );
+}
+
+export default GlobalCssOverride;
