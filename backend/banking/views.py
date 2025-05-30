9b346bfda315cdbd17b792756f8af98a3ed1905b
John Munn - calnet-imac <johnmunn78@gmail.com>
2025-05-30 01:08:45 +0100

    Update api calls to save data to database using PUT and POST methods

--- backend/banking/views.py
+++ backend/banking/views.py
@@ -1,15 +1,13 @@
-from django.shortcuts import render
-from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 from .models import BankAccount
 from .serializers import BankingSerializer
-
 
 # Create your views here.
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PUT'])
 def banking_account_list(request):
     if request.method == 'GET':
         data = BankAccount.objects.all()
@@ -26,11 +24,19 @@ def banking_account_list(request):
             id=request.data['id'], defaults=request.data)
 
         serializer = BankingSerializer(data=request.data)
-
         if serializer.is_valid():
             if data[1] is True:
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'PUT':
+        serializer = BankingSerializer(data=request.data)
+        if serializer.is_valid():
+            bankAccount = BankAccount.objects.get(id=request.data['id'])
+            serializer.update(bankAccount, serializer.data)
+            return Response(status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- backend/customers/views.py
+++ backend/customers/views.py
@@ -16,13 +16,19 @@ def customers_list(request):
                                         context={'request': request},
                                         many=True)
 
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == 'POST':
+
+        data = Customer.objects.update_or_create(
+            id=request.data['id'], defaults=request.data)
+
         serializer = CustomerSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
+            if data[1] is True:
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
